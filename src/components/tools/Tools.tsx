1840bb32992dfae2f7eb2c1b372755527e72da6e
Nikola Badjevic <Ronin95@users.noreply.github.com>
2023-11-04 00:23:09 +0100

    working on ai tools

--- src/components/tools/Tools.tsx
+++ src/components/tools/Tools.tsx
@@ -1,7 +1,7 @@
 import ToolsImg from '../../assets/img/tools.png';
-import converse from '../../assets/img/conversePDF.png';
-import websearch from '../../assets/img/web-search.png';
-import agent from '../../assets/img/agentAI.png';
+import pdfchat from '../../assets/img/conversePDF.png';
+import aiwebsearch from '../../assets/img/web-search.png';
+import aiagent from '../../assets/img/agentAI.png';
 import './ToolsStyle.css';
 
 function Tools() {
@@ -14,15 +14,15 @@ function Tools() {
             </div>
             <div className="tools-bar">
                 <div className="files-styles">
-                    <img className="menu-img-style" src={converse} alt="converse-PDF" />
-                    <h2>Converse with files</h2>
+                    <img className="menu-img-style" src={pdfchat} alt="converse-PDF" />
+                    <h2>PDF Chat</h2>
                 </div>
                 <div className="files-styles">
-                    <img className="menu-img-style" src={websearch} alt="search" />
-                    <h2>Web Search</h2>
+                    <img className="menu-img-style" src={aiwebsearch} alt="search" />
+                    <h2>AI Web Search</h2>
                 </div>
                 <div className="files-styles">
-                    <img className="menu-img-style" src={agent} alt="agent" />
+                    <img className="menu-img-style" src={aiagent} alt="agent" />
                     <h2>AI Agent</h2>
                 </div>
             </div>
