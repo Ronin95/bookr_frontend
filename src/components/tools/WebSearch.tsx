30d96a1f7236e613a5d8425c5dce118b9e1d60d4
Nikola Badjevic <Ronin95@users.noreply.github.com>
2024-01-29 00:18:12 +0100

    finished frontend

--- src/components/tools/WebSearch.tsx
+++ src/components/tools/WebSearch.tsx
@@ -1,22 +1,50 @@
 import * as React from 'react';
 import {FormControl} from 'baseui/form-control';
-import {Textarea} from 'baseui/textarea';
+import { Input } from "baseui/input";
 import { Button } from "baseui/button";
 import './WebSearch.css';
 import axios from 'axios';
 
 function WebSearch() {
     const [searchValue, setSearchValue] = React.useState('');
+    const [isLoading, setIsLoading] = React.useState(false);
+    const [searchPerformed, setSearchPerformed] = React.useState(false);
+    const [searchResults, setSearchResults] = React.useState({
+        exa_result: '',
+        wikipedia_result: '',
+        duckduckgo_result: ''
+    });
 
     const handleSearch = () => {
+        setIsLoading(true);
+        setSearchPerformed(true); // Set search performed to true
         axios.post('http://127.0.0.1:8000/websearch/userSearch/', { user_search: searchValue })
-             .then(response => {
-                 console.log(response);
-             })
-             .catch(error => {
-                 console.error('There was an error!', error);
-             });
-        setSearchValue('');
+            .then(response => {
+                fetchLatestResult(); // Fetch the latest result after POST request
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+                setIsLoading(false);
+            });
+    };
+    
+
+    const fetchLatestResult = () => {
+        axios.get('http://127.0.0.1:8000/websearch/userSearch/')
+            .then(response => {
+                if (response.data && response.data.length > 0) {
+                    // Assuming the response is an array of entries sorted by 'id' descending
+                    const latestResult = response.data[0]; // Get the first (latest) entry
+                    setSearchResults(latestResult);
+                } else {
+                    console.log('No search results found.');
+                }
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching latest result:', error);
+                setIsLoading(false);
+            });
     };    
 
     return (
@@ -24,8 +52,8 @@ function WebSearch() {
             <div className='search-btn-style'>
                 <div className='search-style'>
                     <FormControl label="What is your heart seaching for?">
-                        <Textarea
-                            id="textarea-id"
+                        <Input
+                            id="text-value"
                             value={searchValue}
                             onChange={event => setSearchValue(event.currentTarget.value)}
                         />
@@ -33,20 +61,22 @@ function WebSearch() {
                 </div>
                 <div>
                     <Button onClick={handleSearch}>Search</Button>
-                    <p>Loading...</p>
+                    {isLoading && <p>Searching...</p>}
                 </div>
             </div>
-            <div className='search-results-displayed'>
-                <div className="bstyle">
-                    <p>Exa Search Results</p>
+            {!isLoading && searchPerformed && (
+                <div className='search-results-displayed'>
+                    <div className="exa-style">
+                        <p>Exa Search Results: {searchResults.exa_result.replace("content='", '')}</p>
+                    </div>
+                    <div className="wiki-style">
+                        <p>Wikipedia Results: {searchResults.wikipedia_result}</p>
+                    </div>
+                    <div className="duckduckgo-style">
+                        <p>DuckDuckGo Search Results: {searchResults.duckduckgo_result}</p>
+                    </div>
                 </div>
-                <div className="bstyle">
-                    <p>Wikipedia Results</p>
-                </div>
-                <div className="bstyle">
-                    <p>DuckDuckGo Search Results</p>
-                </div>
-            </div>
+            )}
         </div>
     );
 
